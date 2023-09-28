b566cd5144fdf0ad60be0e7e5b2faae85e8fb3df
fabianlaforga21 <90147053+fabianlaforga21@users.noreply.github.com>
2023-09-28 15:12:44 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("hi");
+
+const parallaxSection = document.querySelector('.scrollding');
+let scrollPosition = 0;
+
+window.addEventListener('scroll', () => {
+    scrollPosition = window.scrollY;
+
+
+    parallaxSection.style.backgroundPositionX = `${-scrollPosition}px`;
+});
